8d09c131b8bb8015fce1c59deb7f9045f048ab6b
unknown <cquinayas@gmail.com>
2023-03-02 15:16:29 -0500

    update config.toml

--- pages/01_📈_Visualizacion.py
+++ pages/01_📈_Visualizacion.py
@@ -14,7 +14,7 @@ image = Image.open('scr/STREAMLIT.png')
 
 col1, col2, col3 = st.columns([1,5,1])    
 with col2:
-    st.markdown('<h3 style="text-align: center;">Análisis de alojamientos en Estados Unidos</h3>', unsafe_allow_html=True)   
+    st.markdown('<h3 style="text-align: center;color: white">Análisis de alojamientos en Estados Unidos</h3>', unsafe_allow_html=True)   
 
     #st.markdown('<iframe title="Power_bi_viernes" width="1350" height="650" src="https://app.powerbi.com/view?r=eyJrIjoiMTc2MTU0MTItM2ExMS00Mzc4LWIzM2QtNDdlOGFhOGZiMDFjIiwidCI6ImU0NmQzODYyLTg1OTUtNDVkMS05YjY5LTYzMDc5OGQ4OTAyZCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
 components.iframe(src="https://app.powerbi.com/view?r=eyJrIjoiN2JhNDNhNGEtYjA2Ni00Yjk3LTkwN2EtMWM4ZjY1NWY0ZWM5IiwidCI6ImU0NmQzODYyLTg1OTUtNDVkMS05YjY5LTYzMDc5OGQ4OTAyZCIsImMiOjR9", width=1350, height=630, scrolling=True)
